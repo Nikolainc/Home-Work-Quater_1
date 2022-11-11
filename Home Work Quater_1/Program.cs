3361e38d2d109fbffecd3b6cf5ff26425dc65e6f
nikolainc <nikolai9710@gmail.com>
2022-11-11 23:51:01 +0300

    Написал рабочий код

--- Home Work Quater_1/Program.cs
+++ Home Work Quater_1/Program.cs
@@ -1,7 +1,58 @@
-﻿class Program
+﻿using System.Runtime.CompilerServices;
+
+class Program
 {
+    const int _textlenght = 3;
+
     static void Main(string[] args)
     {
+        string[] array = { "One", "Two", "Three", ":)", "VeryLongString", "123", "1", "", "01234567890"};
+        PrintNewArray(array);
+        Console.WriteLine();
+    }
 
+    static void PrintNewArray(string[] array)
+    {
+        string[] newArray;
+        int newlenght = 0;
+
+        foreach (var item in array)
+        {
+            if (IslenghtString(item))
+            {
+                newlenght++;
+            }
+        }
+
+        newArray = new string[newlenght];
+
+        foreach (var item in array)
+        {
+            if (IslenghtString(item))
+            {
+                newlenght--;
+                newArray[newlenght] = item;
+            }
+        }
+        newArray.Print();
+    }
+    static bool IslenghtString(string text)
+    {
+        if (text.Length <= _textlenght)
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
+static class MyExtention
+{
+    public static void Print(this string[] array)
+    {
+        foreach (var item in array)
+        {
+            Console.Write(item + "\t");
+        }
     }
 }
